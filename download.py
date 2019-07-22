4a20acf295340369f1bd2891c1ad5f64648ce2c0
calllivecn <c-all@qq.com>
2019-07-22 18:12:44 +0800

    save 修改：     MCLauncher.py 新文件：   download.py

---
+++ download.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2019-07-22 17:40:37
+# author calllivecn <c-all@qq.com>
+
+
+
+
+
+from urllib.request import urlopen
+
+
+def download(url, savepath):
+
+
+
+
