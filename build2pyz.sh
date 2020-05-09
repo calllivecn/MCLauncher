ea14b31b7ac3a30d654f81a5e2f9f523a9c4c573
calllivecn <c-all@qq.com>
2020-05-09 20:00:29 +0800

    修改：     build2pyz.sh

--- build2pyz.sh
+++ build2pyz.sh
@@ -2,10 +2,17 @@
 # date 2019-07-24 14:59:46
 # author calllivecn <c-all@qq.com>
 
+set -e
+
+exit_clear(){
+	rm -rf "$1"
+}
 
 temp=$(mktemp -d)
 
+trap "exit_clear" SIGINT SIGTERM ERR EXIT
+
 cp -v *.py "$temp"
 
-python3 -m zipapp "$temp" -m "main:main" -o MCLauncher.pyz -p "/usr/bin/env python3"
+python3 -m zipapp "$temp" -c -m "main:main" -o MCLauncher.pyz -p "/usr/bin/env python3"
 
